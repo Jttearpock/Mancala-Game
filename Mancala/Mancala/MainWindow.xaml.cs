24957a9afe02d5a6d9f15a1e9d3922a2f01cff44
Jttearpock <43620244+Jttearpock@users.noreply.github.com>
2018-10-24 19:58:40 -0500

    GameState Class + More
    GameState Class and fields created.
    TakeMove() Added
    UpdateValues() Added but Incomplete
    Buttons Added

---
+++ Mancala/Mancala/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Mancala
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using MyNamespace;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Instantiate the currentGame GameState class
+        /// </summary>
+        private GameState currentGame = new GameState();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Method that sets the game board objects to match the array values
+        /// </summary>
+        public void UpdateValues()
+        {
+            for (int x = 0; x < 14; x++)
+            {
+                // TODO Update the string to match correct naming Convention
+                string objectName = "Name" + x;
+                TextBox currentBox = FindName(objectName) as TextBox;
+
+                // TODO Make sure this part works
+                // The ToolTip is an Object not Text and I have no idea if either of these will work. Testing needed.
+                currentBox.ToolTip = this.currentGame.ArrGameBoard[x].ToString();
+            }
+        }
+
+        /// <summary>
+        /// Method that actually takes the turn and changes the values
+        /// </summary>
+        /// <param name="currentBox">The object that initiated the event.</param>
+        public void TakeMove(TextBox currentBox)
+        {
+            int count = int.Parse(currentBox.ToolTip.ToString());
+            string startPos = currentBox.Name.Substring(4, 1);
+            int position = int.Parse(startPos);
+            for (int x = count; x > 0; x--)
+            {
+                position++;
+                if (position == 6 && this.currentGame.PlayerOneTurn == false)
+                {
+                    position++;
+                }   
+                
+                if (position == 13 && this.currentGame.PlayerOneTurn == true)
+                {
+                    position++;
+                }
+
+                if (position > 13)
+                {
+                    position = 0;
+                }
+
+                this.currentGame.ArrGameBoard[position]++;
+            }
+
+            this.UpdateValues();
+        }
+
+        /// <summary>
+        /// Click event that exits program with confirmation check
+        /// </summary>
+        /// <param name="sender">The object that initiated the event.</param>
+        /// <param name="e">The event arguments for the event.</param> 
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to quit?", "Confirm", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+            {
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// Click event that starts a new puzzle with confirmation check
+        /// Doesn't seek confirmation if no game has been started
+        /// </summary>
+        /// <param name="sender">The object that initiated the event.</param>
+        /// <param name="e">The event arguments for the event.</param>
+        private void NewGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.currentGame.OnGoingGame == true)
+            {
+                if (MessageBox.Show("Start new game?", "Confirm", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+                {
+                    this.currentGame.SetStartValues();
+                }
+            }
+            else
+            {
+                this.currentGame.SetStartValues();
+            }
+        }
+
+        /// <summary>
+        /// Click event that resets puzzle with confirmation check
+        /// </summary>
+        /// <param name="sender">The object that initiated the event.</param>
+        /// <param name="e">The event arguments for the event.</param>
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.currentGame.OnGoingGame == true)
+            {
+                if (MessageBox.Show("Restart current puzzle?", "Confirm", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+                {
+                    this.currentGame.SetStartValues();
+                }
+            }
+            else if (this.currentGame.OnGoingGame == false)
+            {
+                this.currentGame.SetStartValues();
+            }
+        }
+    }
+}
