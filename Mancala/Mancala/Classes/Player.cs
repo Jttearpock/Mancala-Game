adf37fb87c7954fb8c891090befaa101693c0552
karyssav <43622797+karyssav@users.noreply.github.com>
2018-11-07 15:06:50 -0600

    Add files via upload

--- Mancala/Mancala/Classes/Player.cs
+++ Mancala/Mancala/Classes/Player.cs
@@ -8,6 +8,26 @@ namespace Mancala
 {
     class Player
     {
+        private string name;
+        private int playerNum;
+        private bool AI;
 
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int PlayerNum
+        {
+            get { return playerNum; }
+            set { playerNum = value; }
+        }
+
+        public bool Ai
+        {
+            get { return AI; }
+            set { AI = value; }
+        }
     }
 }
