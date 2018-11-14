f9d957a04b66f6a18f2b9be3b4d10bd42059eee3
cole-unsinn <cole.unsinn@mymail.nwtc.edu>
2018-11-13 19:56:31 -0600

    player class
    Changed the FindBestMove() and SelectMove() methods so they should work for the hard computer AI

--- Mancala/Mancala/Classes/Player.cs
+++ Mancala/Mancala/Classes/Player.cs
@@ -15,7 +15,7 @@ namespace Mancala
         public Player(string name, bool? isAi)
         {
             this.isAI = isAi;
-            if (isAi ==  true)
+            if (isAi == true)
             {
                 string aiName = "[AI]";
                 this.name = aiName += name;
@@ -39,8 +39,8 @@ namespace Mancala
 
         public int FindBestMove(string[] currentBoard, bool playerOneTurn)
         {
-            
-            if (playerOneTurn)
+
+            if (playerOneTurn == true)
             {
                 for (int i = 0; i < 6; i++)
                 {
@@ -48,40 +48,120 @@ namespace Mancala
                     int endPos = i + count;
                     if ((i + int.Parse(currentBoard[i])) == 6)
                     {
-                        bestMoveArray[i] = 4;
+                        bestMoveArray[i] = 2;
                     }
 
-                    else if (endPos == 0 && int.Parse(currentBoard[12-endPos]) != 0)
+                    else if (endPos == 0 && int.Parse(currentBoard[12 - endPos]) != 0)
                     {
-                        bestMoveArray[i] = 5;
+                        bestMoveArray[i] = 1;
+                    }
+                    else if (count > 2 && endPos >= 0)
+                    {
+                        bestMoveArray[i] = 3;
+                    }
+                    else
+                    {
+                        int random;
+                        Random rand = new Random();
+                        random = rand.Next(6) + 0;
+                        bestMoveArray[random] = 4;
                     }
                 }
 
             }
             else
             {
-                for (int i = 0; i < 6; i++)
+                for (int i = 7; i < 13; i++)
                 {
                     int count = int.Parse(currentBoard[i]);
                     int endPos = i + count;
-                    if ((i + int.Parse(currentBoard[i])) == 6)
+                    if ((i + int.Parse(currentBoard[i])) == 13)
                     {
-                        bestMoveArray[i] = 4;
+                        bestMoveArray[i - 7] = 2;
                     }
 
-                    else if (endPos == 0 && int.Parse(currentBoard[12 - endPos]) != 0)
+                    else if (endPos == 7 && int.Parse(currentBoard[12 - endPos]) != 0)
                     {
-                        bestMoveArray[i] = 5;
+                        bestMoveArray[i - 7] = 1;
                     }
+                    else if (count > 2 && endPos >= 7)
+                    {
+                        bestMoveArray[i - 7] = 3;
+                    }
+                    else
+                    {
+                        int random;
+                        Random rand = new Random();
+                        random = rand.Next(6) + 0;
+                        bestMoveArray[random + 7] = 4;
+                    }
+
                 }
 
             }
-            return 0;
+            return SelectMove(playerOneTurn);
         }
 
         public int SelectMove(bool playerOneTurn)
         {
-            return 0;
+
+            //check the for the best move 
+            int value = 0;
+            if (bestMoveArray.Contains(1))
+            {
+                for (int x = 0; x < 6; x++)
+                {
+                    if (bestMoveArray[x] == 1)
+                    {
+                        value = x;
+                        break;
+                    }
+                }
+
+            }
+            else if (bestMoveArray.Contains(2))
+            {
+                for (int x = 0; x < 6; x++)
+                {
+                    if (bestMoveArray[x] == 2)
+                    {
+                        value = x;
+                        break;
+                    }
+                }
+            }
+            else if (bestMoveArray.Contains(3))
+            {
+                for (int x = 0; x < 6; x++)
+                {
+                    if (bestMoveArray[x] == 3)
+                    {
+                        value = x;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int x = 0; x < 6; x++)
+                {
+                    if (bestMoveArray[x] == 4)
+                    {
+                        value = x;
+                        break;
+                    }
+                }
+            }
+
+
+            if (playerOneTurn)
+            {
+                return value;
+            }
+            else
+            {
+                return (value + 7);
+            }
         }
     }
 }
